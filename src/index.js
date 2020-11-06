b1d9b5ef2b44453d7a9f64472bf35695dc0da6ed
alencartha <alencar_tha@hotmail.com>
2020-11-06 20:37:31 -0300

    final

--- src/index.js
+++ src/index.js
@@ -1,3 +1,60 @@
-import cipher from './cipher.js';
+const ASC = Number (65);
+const sizeAlphabet = Number(26);
+const buttonCipher = document.getElementById("buttonCipher");
+buttonCipher.addEventListener("click", cipher);
+const buttonDecipher = document.getElementById("buttonDecipher");
+buttonDecipher.addEventListener("click", decipher);
 
-console.log(cipher);
+
+function cipher () {
+  
+  let textUser = document.getElementById("messageuser").value.toUpperCase();
+  let offset = Number(document.getElementById("offset").value);
+  let codedWord ="";
+
+  if(textUser==""){
+     alert("Digite os dados para serem Codificados");
+  }
+
+  for (let i = 0; i < textUser.length; i++) {      
+    let codeChar = textUser.charCodeAt(i);
+
+    if (codeChar>= 48 && codeChar <= 57){
+      document.getElementById("messageuser").value=" "; 
+      document.getElementById("textareaCoded").value=" ";   
+      alert("Digite apenas letras");
+    }
+    if (codeChar>= 65 && codeChar <= 90){
+      codedWord+=String.fromCharCode((codeChar- ASC + offset) % sizeAlphabet + ASC);
+      document.getElementById("textareaCoded").innerHTML=codedWord; 
+    } 
+  }
+return codedWord;
+}
+
+function decipher(){
+  
+  let textUser = document.getElementById("messagecontrol").value.toUpperCase();
+  let offset = Number(document.getElementById("offset").value);
+  let decodedWord ="";
+
+  if(textUser==""){
+    alert("Digite os dedos para serem Decodificados");
+  }
+
+  for (let i = 0; i < textUser.length; i++) {
+              
+    let codeChar = textUser.charCodeAt(i);
+    if (codeChar>= 48 && codeChar <= 57){
+      document.getElementById("messageuser").value=" "; 
+      document.getElementById("textareaCoded").value=" ";   
+      alert("Digite apenas letras");
+    }
+    if (codeChar>= 65 && codeChar <= 90){
+      decodedWord+= String.fromCharCode((codeChar- ASC - offset) % sizeAlphabet + ASC);
+      document.getElementById("textareaDecoded").innerHTML=decodedWord;
+    }
+  }
+return decodedWord;
+}
+
